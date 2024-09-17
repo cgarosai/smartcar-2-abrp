76efdb6a93ec2ac05a3968304d955f6c60c648f8
Camille Garo Sail <camille@parrot.hive.palinuro.cloud>
2024-09-17 13:22:00 +0000

    exemple start sh addition

---
+++ exemple_start.sh
@@ -0,0 +1,6 @@
+source smartcar-abrp/bin/activate
+export SMARTCAR_CLIENT_ID=<Your client id here>
+export SMARTCAR_CLIENT_SECRET=<Your client secret here
+export SMARTCAR_REDIRECT_URI=http://localhost:5000/get_access
+
+flask run
